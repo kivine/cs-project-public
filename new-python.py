ca6b28a78bee126c6a63d12a3595b681be66eded
kavya.bansal <kavya.bansal@gmail.com>
2023-05-28 15:58:06 +0530

    0

---
+++ new-python.py
@@ -0,0 +1,18 @@
+import pymysql
+'''x=print('do you want to create a database? If yes,enter y. if you want to use an existing database, enter n')
+if x=='y' or x=='Y':
+    db1=input('enter database name')
+    db.pymysql.connect(host='localhost',user='root',password="")
+    cur=db.cursor()
+    cur.execute('create database'+db1)
+    print('database created')
+else:
+    db2=input('enter database name')
+    db=pymysql.connect(host='localhost',user='root',password="",db = '+db2+')
+    print('connected successfully')'''
+
+db1=input('enter database name ')
+db=pymysql.connect(host='localhost',user='root',password='root')
+cur=db.cursor()
+cur.execute('create database'+db1)
+print('database created')
